af1b8c52cf14733b0e27172b3f016b75c4b00bf5
mepnotme <gnadormep@gmail.com>
2025-05-28 20:18:07 +0200

    integrado el salto con la curva del bloqr

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from math import fabs
 
 WORLD_WIDTH = 256
 WORLD_HEIGHT = 256
@@ -11,10 +12,8 @@ HERO_IMG_SHIFT_X = -105
 HERO_IMG_SHIFT_Y = -112
 BLOCK_IMG_SHIFT_X = -105
 BLOCK_IMG_SHIFT_Y = -112
-
-pyramid = [] # matriz que contiene información sobre todos los bloques del juego
-hero_floor = 0
-hero_col = 0
+FRAMES_JUMPING = 6
+JUMP_HEIGHT = 15    # altura máxima del salto del jugador (en píxeles)
 
 def mi_error(mensaje):
     print(f"ERROR: {mensaje}")
@@ -29,7 +28,7 @@ def get_new_scale_factor(display_x_resolution, display_y_resolution):
     return new_scale_factor
 
 def init_game():
-    global pyramid, hero_floor, hero_col
+    global pyramid, hero
     pyramid = []
     floor_columns = 7
     for floor_number in range(7):
@@ -38,8 +37,9 @@ def init_game():
             floor.append(0)
         pyramid.append(floor)
         floor_columns -= 1
-    hero_floor = 6
-    hero_col = 0
+    hero["floor"] = 6
+    hero["col"] = 0
+    hero["state"] = "idle"
 
 def screen_menu():
     global screen, scale_factor
@@ -84,14 +84,22 @@ def cell_exists(row, col):
     return 0 <= row <= 6 and 0 <= col <= 6 - row
 
 
-def draw_hero(hero_floor, hero_col):
-    hero_x, hero_y = coordinates_of_block(hero_floor, hero_col)
-    draw_surface.blit(hero_img, (hero_x + HERO_IMG_SHIFT_X, hero_y + HERO_IMG_SHIFT_Y))
-
+def draw_hero(hero):
+    hero_x, hero_y = coordinates_of_block(hero["floor"], hero["col"])
+    if hero["state"] == "jumping":
+        hero_dest_x, hero_dest_y = coordinates_of_block(hero["dest_floor"], hero["dest_col"])
+        inc_x = (frame_counter - hero["state_init_counter"]) * (hero_dest_x - hero_x) / FRAMES_JUMPING
+        inc_y = (frame_counter - hero["state_init_counter"]) * (hero_dest_y - hero_y) / FRAMES_JUMPING
+        curve_y_inc = (1 - (1/(FRAMES_JUMPING / 2)) * fabs( frame_counter - hero["state_init_counter"] - 3)) * JUMP_HEIGHT
+        print(curve_y_inc)
+    else:
+        inc_x = 0
+        inc_y = 0
+        curve_y_inc = 0
+    draw_surface.blit(hero["img"], (hero_x + HERO_IMG_SHIFT_X + inc_x, hero_y + HERO_IMG_SHIFT_Y + inc_y - curve_y_inc))
 
 def screen_match():
     global scale_factor, hero_floor, hero_col
-
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -100,27 +108,34 @@ def screen_match():
             new_x_resolution, new_y_resolution = event.dict['size']
             scale_factor = get_new_scale_factor(new_x_resolution, new_y_resolution)
         elif event.type == pygame.KEYDOWN:
-            dest_hero_row = hero_floor
-            dest_hero_col = hero_col
-            if event.key == K_q:  # Diagonal arriba-izquierda
-                dest_hero_row = hero_floor + 1
-                dest_hero_col = hero_col - 1
-            elif event.key == K_w:  # Diagonal arriba-derecha
-                dest_hero_row = hero_floor + 1
-                dest_hero_col = hero_col
-            elif event.key == K_a:  # Diagonal abajo-izquierda
-                dest_hero_row = hero_floor - 1
-                dest_hero_col = hero_col
-            elif event.key == K_s:    # Diagonal abajo-derecha
-                dest_hero_row = hero_floor - 1
-                dest_hero_col = hero_col + 1
-            if cell_exists(dest_hero_row, dest_hero_col):
-                hero_floor = dest_hero_row
-                hero_col = dest_hero_col
+            dest_hero_floor = hero["floor"]
+            dest_hero_col = hero["col"]
+            if event.key == K_q and hero["state"] == "idle":  # Diagonal arriba-izquierda
+                dest_hero_floor = hero["floor"] + 1
+                dest_hero_col = hero["col"] - 1
+            elif event.key == K_w and hero["state"] == "idle":  # Diagonal arriba-derecha
+                dest_hero_floor = hero["floor"] + 1
+                dest_hero_col = hero["col"]
+            elif event.key == K_a and hero["state"] == "idle":  # Diagonal abajo-izquierda
+                dest_hero_floor = hero["floor"] - 1
+                dest_hero_col = hero["col"]
+            elif event.key == K_s and hero["state"] == "idle":  # Diagonal abajo-derecha
+                dest_hero_floor = hero["floor"] - 1
+                dest_hero_col = hero["col"] + 1
+            if (hero["floor"] != dest_hero_floor or hero["col"] != dest_hero_col) and cell_exists(dest_hero_floor,                                                                                  dest_hero_col):
+                hero["dest_floor"] = dest_hero_floor
+                hero["dest_col"] = dest_hero_col
+                hero["state"] = "jumping"
+                hero["state_init_counter"] = frame_counter  # contador para saber cuánto tiempo llevamos en este estado
+    if hero["state"] == "jumping":
+        if frame_counter - hero["state_init_counter"] > FRAMES_JUMPING:
+            hero["floor"] = hero["dest_floor"]
+            hero["col"] = hero["dest_col"]
+            hero["state"] = "idle"
 
     draw_surface.fill((0, 0, 0))
     draw_pyramid()
-    draw_hero(hero_floor, hero_col)
+    draw_hero(hero)
 
 
 pygame.init()
@@ -139,8 +154,17 @@ menu_title_img = font_title.render("rufbrt", False, (204, 96, 120))
 menu_subtitle_img = font_paragraph.render("press space to start", False, (116, 220, 188))
 block_img = pygame.image.load('assets/block_1.png')
 
-hero_img = pygame.image.load('assets/hero_1.png')
+pyramid = []    # matriz que contiene información sobre todos los bloques del juego
+hero = {
+    "floor": 0,         # piso en el que se encuentra el héroe
+    "col": 0,           # columna en la que se encuentra el héroe
+    "state": "idle",    # estado en el que se encuentra el héroe ("idle", "jumping"...)
+    "img": pygame.image.load('assets/hero_1.png'),
+}
+
+frame_counter = 0
 screen = "menu"
+clock = pygame.time.Clock()
 while True:  # main game loop
     if screen == "menu":
         screen_menu()
@@ -153,3 +177,5 @@ while True:  # main game loop
     display_surface.fill((0, 0, 0))
     display_surface.blit(scaled_draw_surface, ((display_surface.get_width() - scaled_draw_surface.get_width()) / 2, (display_surface.get_height() - scaled_draw_surface.get_height()) / 2))
     pygame.display.update()
+    clock.tick(30)
+    frame_counter += 1
